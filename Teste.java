f8be1c24820c7be070c7df188112ac0e0f308687
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2022-05-15 17:53:17 +0000

    Exercicio 1

--- Teste.java
+++ Teste.java
@@ -13,7 +13,32 @@ public class Teste {
         System.out.println(emp.getCodigoSetor());
         emp.calcularSalario();
         
+        Operario ope = new Operario();
+        ope.setNome("Diego");
+        ope.setEndereco("Rua x 123");
+        ope.setTelefone("367289");
+        ope.setCodigoSetor(453);
+        ope.setSalarioBase(1460.90);
+        ope.setImpostos(5.90);
+        ope.setComissao(340.00);
+        System.out.println(ope.getNome());
+        System.out.println(ope.getEndereco());
+        System.out.println(ope.getTelefone());
+        System.out.println(ope.getCodigoSetor());
+        ope.calcularSalario();
 
-
+        Administrador adm = new Administrador();
+        adm.setNome("Maria");
+        adm.setEndereco("Rua Y 23");
+        adm.setTelefone("3454567");
+        adm.setCodigoSetor(230);
+        adm.setSalarioBase(6450.90);
+        adm.setImpostos(65.90);
+        adm.setAjudaCusto(250);
+        System.out.println(adm.getNome());
+        System.out.println(adm.getEndereco());
+        System.out.println(adm.getTelefone());
+        System.out.println(adm.getCodigoSetor());
+        adm.calcularSalario();
     }
 }
